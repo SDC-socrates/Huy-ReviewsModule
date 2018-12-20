a10db7c49a40fadee513d59b99899de666f7b1bf
hgtruong <huygtruong@gmail.com>
2018-12-19 19:45:58 -0800

    Added "addNewUser" function to be used in seeding script

--- database/index.js
+++ database/index.js
@@ -1,1 +1,24 @@
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'reviews'
+});
+
+connection.connect();
+
+// TODO: Do not allow user to update their review if
+//       they already left a review
+const addNewUser = function(id, name, review, rating) {
+  var query = `insert ignore into reviews(id, name, review, rating) values ("${id}", "${name}", "${review}","${rating}")`;
+  connection.query(query, (err) => {
+    if (err) { throw err; }
+    // console.log("Added To DB");
+  });
+}
+
+module.exports = {
+  addNewUser
+};
