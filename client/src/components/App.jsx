171ad2286e93aca89b6fb6a79caa280af00d8d27
hgtruong <huygtruong@gmail.com>
2018-12-24 15:18:38 -0800

    Added "See More Reviews" button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,28 +8,50 @@ class App extends React.Component {
     super(props);
     this.state = {
       reviews: [],
+      reviewCount: 0
     };
   }
 
   componentDidMount() {
     this.getAllReviews();
+    this.getreviewCount();
   }
 
   getAllReviews() {
     axios.get('/api/turash/reviews/:id')
       .then((result) => {
-        this.setState({ reviews: result.data });
+        this.setState({ reviews: result.data.reverse() });
       })
       .catch((err) => {
         if (err) { throw err; }
       });
   }
 
+  getreviewCount() {
+    axios.get('/api/turash/reviews/:id/reviewCount')
+      .then((result) => {
+        console.log('result is', result);
+        this.setState({ reviewCount: result.data[0]['count(*)']});
+      })
+      .catch((err) => {
+        if (err) { throw err; }
+      });
+  }
+
+
+
   // TODO: Function to fetch for specific review
   render() {
     const { reviews } = this.state;
+    const { reviewCount} = this.state;
     return (
       <div className="reviews">
+
+        <div className="reviewLabel"> Reviews </div>
+        <div className="starAndNumOfReviews">
+          <div className="starRating"> Stars </div>
+          <div id="numOfReviews"> - { reviewCount } ratings </div>
+        </div>
         {
           reviews.map((element, key) => (
             <Reviews
@@ -38,6 +60,8 @@ class App extends React.Component {
             />
           ))
         }
+
+        <button className="moreReviews"> See More Reviews </button>
       </div>
     );
   }
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Picture = (props) => (
-  console.log('props', props),
+  // console.log('props', props),
 
   <div className="individualReview">
     <div className="picture"> </div>
