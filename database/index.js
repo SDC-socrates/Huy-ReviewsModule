9b15b768d45b757cb09003777231e87fc06c1828
ccades <hellocaden@gmail.com>
2019-01-10 20:18:52 -0800

    seeding db but only to 34k

--- database/index.js
+++ database/index.js
@@ -1,95 +1,92 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('reviews', 'postgres', 'postgres', {
+const sequelize = new Sequelize('reviews', 'ccades', '', {
   host: 'localhost',
   dialect: 'postgres',
+  port: 5432,
+  pool: {
+    max: 1,
+    min: 0,
+    acquire: 2000000,
+    idle: 10000,
+  },
 });
 
-sequelize.authenticate().then(() => {
-  console.log('CONNECTED!');
-  const Reviews = sequelize.define('reviews', {
-    id: { type: Sequelize.INTEGER },
-    carid: { type: Sequelize.INTEGER },
-    name: { type: Sequelize.STRING },
-    review: { type: Sequelize.STRING },
-    rating: { type: Sequelize.INTEGER },
-    date: { type: Sequelize.STRING },
+const Reviews = sequelize.define('reviews', {
+  id: { type: Sequelize.INTEGER(11111111), primaryKey: true },
+  carid: { type: Sequelize.INTEGER(11111111) },
+  name: { type: Sequelize.STRING(50) },
+  review: { type: Sequelize.STRING(250) },
+  rating: { type: Sequelize.INTEGER(2) },
+  date: { type: Sequelize.STRING(20) },
+}, { timestamps: false });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('CONNECTED!');
+  }).catch((err) => {
+    console.log('ERROR', err);
   });
 
-  Reviews.sync({ force: true }).then(() => {
-    Reviews.create({
 
-    });
-  });
-}).catch((err) => {
-  console.log('ERROR', err);
-});
+sequelize.sync();
 
-// TODO: Do not allow user to update their review if
-//       they already left a review
+const addNewReview = (review) => {
+  let reviewDate = null;
 
-const addNewUser = function(userReview) {
-  // const userId = userReview.userId;
-
-  if (userReview.userReviewDate === null) {
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-
+  if (review.date === null) {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     const newDate = new Date();
-    const month = monthNames[newDate.getMonth()].slice(0,3);
+    const month = monthNames[newDate.getMonth()].slice(0, 3);
     const date = newDate.getDate();
     const year = newDate.getFullYear();
     const dateInfo = `${month} ${date} ${year}`;
-    userReview.userReviewDate = dateInfo;
+    reviewDate = dateInfo;
   }
 
-
-  var query = `insert into reviews(carId, name, review, rating, date) values ("${userReview.carId}", "${userReview.userName}", "${userReview.userReview}","${userReview.userRating}", "${userReview.userReviewDate}")`
+  const query = `INSERT INTO reviews(id, carid, name, review, rating, date) VALUES ("${review.id}", "${review.carid}", "${review.name}", "${review.review}","${review.rating}", "${reviewDate}")`;
 
   sequelize.query(query, (err) => {
     if (err) {
       console.log('Error trying to add user into database.');
-      return;
     }
   });
-}
+};
 
-const getUsers = function(submittedId, endNumForNextSet, callback) {
-
-  var query = `select * from reviews where userId=${submittedId}`;
+const getUsers = (submittedId, endNumForNextSet, callback) => {
+  const query = `select * from reviews where userId=${submittedId}`;
   sequelize.query(query, (err, result) => {
     if (err) {
       console.log('Error retrieving 15 records');
-      return;
     } else {
       callback(err, result);
     }
   });
-}
+};
 
-const getRatingCount = function(submittedId, callback) {
-  var query = `select rating from reviews where userId=${submittedId};`
+const getRatingCount = (submittedId, callback) => {
+  const query = `select rating from reviews where userId=${submittedId}`;
   sequelize.query(query, (err, result) => {
     if (err) {
       console.log('Error getting ratings');
-      return;
     } else {
       callback(err, result);
     }
   });
-}
+};
 
-const getReviewCount = function(submittedId, callback) {
-  var query = `select count(*) from reviews where userId=${submittedId}`;
+const getReviewCount = (submittedId, callback) => {
+  const query = `select count(*) from reviews where userId=${submittedId}`;
   sequelize.query(query, (err, result) => {
     if (err) {
       console.log('Error getting count of reviews');
-      return;
     } else {
       callback(err, result);
     }
   });
-}
+};
 
 module.exports = {
-  addNewUser, getUsers, sequelize, getReviewCount, getRatingCount,
+  Reviews, addNewReview, getUsers, sequelize, getReviewCount, getRatingCount,
 };
--- faker.js
+++ faker.js
@@ -1,27 +1,48 @@
 const faker = require('faker');
 const db = require('./database/index.js');
 
-const insertIntoDb = (numOfTimes = 103) => {
-  var users = [];
-  for (let i = 0; i < numOfTimes; i++) {
-    const user = {
-      carId: faker.random.number({
+// const makeReview = () => {
+//   for (let i = 0; i < 10; i += 1) {
+//     db.Reviews.create({
+//       id: i + 1,
+//       carid: faker.random.number({
+//         min: 1,
+//         max: 50,
+//       }),
+//       name: faker.name.findName(),
+//       review: faker.lorem.sentences(3),
+//       rating: faker.random.number({
+//         min: 1,
+//         max: 5,
+//       }),
+//       date: faker.date.recent(5).toString().slice(4, 15),
+//     });
+//   }
+// };
+
+// makeReview();
+
+const insertIntoDb = (numOfTimes = 100000) => {
+  const reviews = [];
+  for (let i = 0; i < numOfTimes; i += 1) {
+    const review = {
+      id: i + 1,
+      carid: faker.random.number({
         min: 1,
-        max: 30,
+        max: 11111111,
       }),
-      userName: faker.name.findName(),
-      userReview: faker.lorem.sentences(3),
-      userReviewDate: faker.date.recent(5).toString().slice(4, 15),
-      userRating: faker.random.number({
+      name: faker.name.findName(),
+      review: faker.lorem.sentences(3),
+      rating: faker.random.number({
         min: 1,
         max: 5,
-      })
-    }
-    users.push(user);
-
+      }),
+      date: faker.date.recent(5).toString().slice(4, 15),
+    };
+    reviews.push(review);
   }
 
-  users.sort(function compare(a, b) {
+  reviews.sort(function compare(a, b) {
     if (a[4] < b[4]) {
       return -1;
     }
@@ -31,9 +52,10 @@ const insertIntoDb = (numOfTimes = 103) => {
     return 0;
   });
 
-  users.forEach( (currentElement) => {
-    db.addNewUser(currentElement);
-  })
+  reviews.forEach((currentElement) => {
+    // console.log('ELEMENTTTTTTT', currentElement);
+    db.Reviews.create(currentElement);
+  });
 };
 
 module.exports = {
