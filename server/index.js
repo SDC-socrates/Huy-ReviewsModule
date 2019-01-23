dbcc3d24e06da32774d9a34dfa55b9a287db0e4c
Chris Caden <hellocaden@gmail.com>
2019-01-22 17:40:36 -0800

    updated seed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
 
 const postgres = require('../database/cassandra.js');
 const app = express();
